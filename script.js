7d28efa4d93ad55581b0a3b96e1313504f888ad6
sabbir2joy <63640161+sabbir2joy@users.noreply.github.com>
2020-05-09 22:29:10 +0600

    coursera assignment

---
+++ script.js
@@ -0,0 +1,25 @@
+(function (window) {
+/*
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
+*/
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i in names) {
+  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
+  if (firstLetter == 'j') {
+     window.byeSpeaker.speak(names[i]);
+  } else {
+     window.helloSpeaker.speak(names[i]);
+  }
+}
+})(window);
